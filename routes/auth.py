685de64150b736c5ee4b5e9e9806a082cbd16d9f
TalhaShaikh-1418 <talhashaikh3408@gmail.com>
2026-06-15 20:10:57 +0530

    Fix MySQL reconnect on login

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
-from utils.db import cursor, db
+from utils.db import get_db
 import random
+from utils.db import cursor, db
 
 # Blueprint create
 auth_bp = Blueprint("auth", __name__)
@@ -71,6 +72,9 @@ def login():
     if request.method == "POST":
         mobile = request.form["mobile"]
         password = request.form["password"]
+
+        if not db.is_connected():
+            db.reconnect(attempts=3, delay=2)
 
         query = """
         SELECT * FROM users
--- utils/db.py
+++ utils/db.py
@@ -4,15 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    autocommit=True
-)
-
-cursor = db.cursor(buffered=True)
+def get_db():
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        autocommit=True
+    )
 
 
 
